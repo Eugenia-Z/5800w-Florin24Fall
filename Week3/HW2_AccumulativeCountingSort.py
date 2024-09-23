28e80e1a2f26c857b4e1751faa3e3044b2f22196
eugenia <e.eugene1201@gmail.com>
2024-09-23 12:19:08 -0700

    CountingSort Accumulative

---
+++ Week3/HW2_AccumulativeCountingSort.py
@@ -0,0 +1,24 @@
+class Solution:
+    def preprocess(nums, k):
+        count = [0]*(k+1)
+        # count original frequency
+        for num in nums:
+            count[num] += 1
+        
+        # count accumulative frequency -> the # of elements that is smaller than num at c-th location 
+        for i in range(2, k):
+            count[i] += count[i-1]
+        return count
+
+    def query(count, a, b):
+        if a == 1:
+            return count[b]
+        return count[b] - count[a-1]
+
+if __name__ == "__main__":
+    nums = [11, 12, 13, 5, 6, 7]
+    sol = Solution
+    count = sol.preprocess(nums,15)
+    res = sol.query(count, 5, 10)
+    print(res)
+    
