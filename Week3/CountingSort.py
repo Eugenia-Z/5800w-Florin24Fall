4e62527493c31b7f443abad82aa2f5cb56a8c57c
eugenia <e.eugene1201@gmail.com>
2024-09-23 15:00:56 -0700

    conuting sort

---
+++ Week3/CountingSort.py
@@ -0,0 +1,26 @@
+def CountingSort(nums):
+    # build the count array. length equals the max value of num + 1
+    length = max(nums) + 1
+    count = [0] * length
+    for num in nums:
+        count[num] += 1
+        
+    # update the count array to hold its position -> to be cumulative
+    for i in range(1, length):
+        count[i] += count[i-1]
+    
+    # initialize the output array
+    res = [0] * len(nums)
+    
+    # fillin the output array in reverse order based on the value in countarray
+    for num in nums:
+        res[count[num]-1] = num
+        count[num] -= 1
+    return res
+
+if __name__ == "__main__":
+    nums = [1,1,4,1,2,3,5,6,4]
+    print(CountingSort(nums))
+            
+        
+        
