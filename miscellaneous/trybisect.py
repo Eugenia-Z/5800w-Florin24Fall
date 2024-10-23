c9f7486fb8e7c37a6f10c20e4a04706a58dcf003
eugenia <e.eugene1201@gmail.com>
2024-10-23 13:51:23 -0700

    updated week7

--- miscellaneous/trybisect.py
+++ miscellaneous/trybisect.py
@@ -1,8 +1,8 @@
 import bisect
 
-nums = [1,2,4,7,8,10]
+nums = [1,2,4,4,4,7,8,10]
 left_pos = bisect.bisect_left(nums,6)
 right_pos = bisect.bisect_right(nums,4)
 
-bisect.insort(nums, 7)
-print(nums)
+# bisect.insort(nums, 7)
+print(right_pos)
