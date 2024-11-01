c58adfef4d00cdf0363dec8dd4f6404bd497db3b
eugenia <e.eugene1201@gmail.com>
2024-11-01 13:23:23 -0700

    added alternative sol to knapsack

--- Week9/Knapsack.py
+++ Week9/Knapsack.py
@@ -1,3 +1,4 @@
+# Prof Florin's Sol: Mathmetical
 def knapsack(v, w, W):
     V = [[0 for x in range(W+1)] for x in range(len(v) + 1)]  # init a 2D table
     for i in range(len(v) + 1):
@@ -9,5 +10,27 @@ def knapsack(v, w, W):
             else:
                 V[i][j] = max(V[i-1][j-w[i-1]] + v[i-1], V[i-1][j])  # here to remember[j-w[i-1]], the previous status doesn't have w[i-1], then for this iteration, we add v[i-1]
     return V[len(v)][W]
-print(knapsack([60, 100, 120],[10, 20, 30], 50))
+# print(knapsack([60, 100, 120],[10, 20, 30], 50))
                 
+# More interpretable sol:
+def knapsack1(values, weights, capacity):
+    n = len(values)
+    
+    # Create a DP array to store maximum value at each capacity
+    dp = [[0 for _ in range(capacity+1)] for _ in range(n+1)]
+    
+    # BUild the DP table in bottom-up matter
+    for i in range(1, n+1):
+        for w in range(capacity + 1):
+            # if including item i exceeds current capacity, exclude it
+            if weights[i-1] > w:
+                dp[i][w] = dp[i-1][w]
+            else:
+                # Max of including the item and excluding the item
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i-1]] + values[i-1])
+    return dp[n][capacity]
+values = [60, 100, 120]
+weights = [10, 20, 30]
+capacity = 50
+
+print("Maximum value:", knapsack(values, weights, capacity))
